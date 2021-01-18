9ca07d6006e3e14e7f90b98c93da0d059fc40540
Karl Horky <karl.horky@gmail.com>
2021-01-18 17:35:58 +0100

    Make link check less strict

--- src/checks/linkOnGithubAbout.ts
+++ src/checks/linkOnGithubAbout.ts
@@ -1,6 +1,6 @@
+import cheerio from 'cheerio';
 import execa from 'execa';
 import fetch from 'node-fetch';
-import cheerio from 'cheerio';
 
 export const title = 'Github Repository contains link on About Section';
 
@@ -11,11 +11,8 @@ export default async function linkOnGithubAbout() {
 
   const html = await (await fetch(repoUrlStdout)).text();
 
-  const urlMatch = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi;
-  const regex = new RegExp(urlMatch);
-
   const $ = cheerio.load(html);
-  const aboutHeading = $('h2')
+  const linkElement = $('h2')
     .filter(function(this: Node) {
       return (
         $(this)
@@ -25,19 +22,14 @@ export default async function linkOnGithubAbout() {
     })
     .next()
     .next()
-    .text()
-    .trim();
+    .children('.octicon.octicon-link')
+    .next()
+    .children('a[href]');
 
-  if (!aboutHeading) {
+  if (linkElement.length === 0) {
     throw new Error(`Project link not found on about section:
     - Make sure you add the link to the repo on the About section.
     - You will see an link 🔗 icon before the url if is the right place.
-    `);
-  } else if (!aboutHeading.match(regex)) {
-    throw new Error(`Project link is not a link:
-    - ${aboutHeading} is not a valid Url.
-    - Check that you have an actual link on your repo About section.
-    - You should see icon 🔗  before the url.
     `);
   }
 }
