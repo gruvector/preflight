af405fe5523f0dbe118cad2713b76d28d3873c09
Karl Horky <karl.horky@gmail.com>
2021-02-03 10:49:57 +0100

    Ignore .replit file when running in Repl.it

--- src/checks/allChangesCommittedToGit.ts
+++ src/checks/allChangesCommittedToGit.ts
@@ -3,6 +3,14 @@ import execa from 'execa';
 export const title = 'All changes committed to Git';
 
 export default async function allChangesCommittedToGit() {
+  const { stdout: replSlug } = await execa.command('echo $REPL_SLUG');
+
+  const isRunningInReplIt = replSlug !== '';
+
+  if (isRunningInReplIt) {
+    await execa.command('echo ".replit" >> .git/info/exclude');
+  }
+
   const { stdout } = await execa.command('git status --porcelain');
 
   if (stdout !== '') {
