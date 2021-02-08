4e2b67913f66f4d2aa4e4326b7f36afadc0eb129
José Fernando Höwer Barbosa <61600906+Josehower@users.noreply.github.com>
2021-02-08 16:27:47 +0100

    Add prettier exceptions for next app (#27)
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- src/checks/prettier.ts
+++ src/checks/prettier.ts
@@ -29,6 +29,12 @@ export default async function prettierCheck() {
       // File from create-react-app not matching our formatting
       // https://github.com/facebook/create-react-app/blob/fdbde1f3c256b43d5386b5ae3a75083dbd8f0aff/packages/cra-template/template/src/reportWebVitals.js
       /src[/\\]reportWebVitals\.js$/,
+
+      // Files from create-next-app not matching our formatting
+      // https://github.com/vercel/next.js/blob/5a73859fe8d3659d52e169f97188ae422e0d2ace/packages/create-next-app/templates/default/pages/api/hello.js
+      /pages[/\\]api[/\\]hello\.js$/,
+      // https://github.com/vercel/next.js/blob/aacfa79ddf88e4e1488b63ac253af27110f0a151/packages/create-next-app/templates/default/pages/_app.js
+      /pages[/\\]_app\.js$/,
     ];
 
     const unformattedFiles = normalizeNewline(error.stdout)
