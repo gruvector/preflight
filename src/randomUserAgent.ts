ef3d8212c186a5ca134b20726e92c9c7c87b2a1f
Karl Horky <karl.horky@gmail.com>
2021-02-09 16:30:08 +0100

    Fix random index

--- src/randomUserAgent.ts
+++ src/randomUserAgent.ts
@@ -1,6 +1,6 @@
 import userAgents from 'top-user-agents';
 
 export default function randomUserAgent() {
-  const randomIndex = Math.random() * (userAgents.length - 1);
+  const randomIndex = Math.floor(Math.random() * (userAgents.length - 1));
   return userAgents[randomIndex];
 }
