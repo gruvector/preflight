c753bb59ed7dc5f24af2c7b7eafbd2de61551ec2
Karl Horky <karl.horky@gmail.com>
2021-02-09 16:27:10 +0100

    Upgrade to ESM version of top-user-agents

--- src/randomUserAgent.ts
+++ src/randomUserAgent.ts
@@ -1,6 +1,4 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const userAgents = require('top-user-agents');
+import userAgents from 'top-user-agents';
 
 export default function randomUserAgent() {
   const randomIndex = Math.random() * (userAgents.length - 1);
