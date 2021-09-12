257bddad4e8d802a1e6be8f63d20a17a305278f1
Karl Horky <karl.horky@gmail.com>
2021-09-12 18:25:41 +0200

    Ignore Babel ESLint parser package

--- src/checks/noDependencyProblems/noUnusedDependencies.ts
+++ src/checks/noDependencyProblems/noUnusedDependencies.ts
@@ -20,6 +20,7 @@ export default async function noUnusedAndMissingDependencies() {
     'tailwindcss',
 
     // ESLint configuration
+    '@babel/eslint-parser',
     '@next/eslint-plugin-next',
     '@typescript-eslint/*',
     '@upleveled/eslint-config-upleveled',
