5949ae7d51c1e322d541f265dbddd50c3c2ad00e
gruvector <vector.gru316@gmail.com>
2023-01-19 22:27:30 +0100

    Update dependency upgrades - non-major (#352)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- src/util/commandExample.ts
+++ src/util/commandExample.ts
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
 
 // https://www.compart.com/en/unicode/U+2800
+// eslint-disable-next-line security/detect-bidi-characters -- Intentional use of unusual character for formatting
 const emptyBrailleCharacter = '‎';
 
 export default function commandExample(command: string) {
