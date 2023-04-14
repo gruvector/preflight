ea9c9c720a21cc98e144755f7421818554a61189
gruvector <vector.gru316@gmail.com>
2023-04-14 09:43:50 +0200

    Replace dependency @upleveled/eslint-config-upleveled with eslint-config-upleveled 4.0.12 (#380)Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com> Co-authored-by: Karl Horky <karl.horky@gmail.com>
    * Replace dependency @upleveled/eslint-config-upleveled with eslint-config-upleveled 4.0.12
    
    * Replace dependency @upleveled/eslint-plugin-upleveled with eslint-plugin-upleveled 2.1.7 (#381)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Karl Horky <karl.horky@gmail.com>
    
    * Update .eslintrc.cjs
    
    * Update .eslintrc.cjs
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- docker/.eslintrc.cjs
+++ docker/.eslintrc.cjs
@@ -1,6 +1,6 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
-  extends: ['@upleveled/upleveled'],
+  extends: ['upleveled'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
