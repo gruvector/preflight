71ea0875a0fc8d8c59bc6adf5f9fcbf3e330db49
Karl Horky <karl.horky@gmail.com>
2023-05-11 20:36:01 +0200

    Simplify linting and type checking (#402)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,14 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
   extends: ['upleveled'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.json',
+      './tsconfig.nonsrc.json',
+      './bin/tsconfig.json',
+    ],
+  },
 };
 
 module.exports = config;
