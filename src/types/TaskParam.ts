13aee3730154fe21a9bd91280088e824ad5cc871
gruvector <vector.gru316@gmail.com>
2023-09-27 16:53:18 +0000

    Update dependency listr2 to v7 (#445)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- src/types/TaskParam.ts
+++ src/types/TaskParam.ts
@@ -1,3 +1,7 @@
 import { ListrDefaultRenderer, ListrTaskWrapper } from 'listr2';
 
-export type TaskParam = ListrTaskWrapper<any, ListrDefaultRenderer>;
+export type TaskParam = ListrTaskWrapper<
+  any,
+  ListrDefaultRenderer,
+  ListrDefaultRenderer
+>;
